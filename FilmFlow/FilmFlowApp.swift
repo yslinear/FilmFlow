23cb7b4a70fffb67b2603c6d80ca19fa60e17318
Ying-Shan Lin <yslinear@gmail.com>
2024-03-30 14:02:37 +0800

    feat: 新增FilmFlow Xcode專案結構與SwiftUI界面

---
+++ FilmFlow/FilmFlowApp.swift
@@ -0,0 +1,32 @@
+//
+//  FilmFlowApp.swift
+//  FilmFlow
+//
+//  Created by Ying-Shan Lin on 2024/3/30.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FilmFlowApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FilmFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FilmFlow
+//
+//  Created by Ying-Shan Lin on 2024/3/30.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
