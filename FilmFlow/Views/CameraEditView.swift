7431e6790db1d7cc07f0c441c523bca25abd3b12
Ying-Shan Lin <yslinear@gmail.com>
2024-03-31 12:49:38 +0800

    refactor(views): 更新 CameraEditView 和 InfoView 位置

--- FilmFlow/Views/CameraEditView.swift
+++ FilmFlow/Views/CameraEditView.swift
@@ -11,12 +11,24 @@ import SwiftData
 
 struct CameraEditView: View {
     @Bindable var camera: Camera
+    @State private var maxShutterSpeed = 0
 
     var body: some View {
         Form {
             Section(header: Text("Camera Info")) {
                 TextField("Name", text: $camera.name)
-                DatePicker("Timestamp", selection: $camera.timestamp)
+            }
+            // TODO: Add more fields
+            Section(header: Text("Camera Settings")) {
+                Picker("Max Shutter Speed", selection: $maxShutterSpeed) {
+                    Text("8000").tag(8000)
+                    Text("4000").tag(4000)
+                    Text("2000").tag(2000)
+                    Text("1000").tag(1000)
+                    Text("500").tag(500)
+                    Text("250").tag(250)
+                }
+                .pickerStyle(.segmented)
             }
         }
     }
