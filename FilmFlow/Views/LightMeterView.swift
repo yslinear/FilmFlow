62e4eb83dd755c30e9bb017419ca8942c77aad87
Ying-Shan Lin <yslinear@gmail.com>
2024-04-05 02:23:04 +0800

    refactor(影像顯示): 優化 LightMeterView 結構與導入AVFoundation

--- FilmFlow/Views/LightMeterView.swift
+++ FilmFlow/Views/LightMeterView.swift
@@ -8,29 +8,24 @@
 import Foundation
 import SwiftUI
 import SwiftData
+import AVFoundation
 
 struct LightMeterView: View {
     @StateObject var manager = CameraManager()
 
-    var image: CGImage?
-
     var body: some View {
-        Grid {
-            GridRow {
-                if let image = manager.frame {
-                    Image(image, scale: 1.0, orientation: .up, label: Text("frame"))
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 240, height: 240)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                } else {
-                    Color.red
-                }
-            }
-            Divider()
-            GridRow {
+        if let image = manager.frame {
+            VStack() {
+                Image(image, scale: 1.0, orientation: .up, label: Text("frame"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: 300)
                 Text("ISO: \(manager.videoDevice?.iso ?? 0)")
+                Text("Aperture: \(Float((manager.videoDevice?.lensAperture)!))")
+                Text("Sutter Speed: \(Float(CMTimeGetSeconds((manager.videoDevice?.exposureDuration)!)))")
             }
+        } else {
+            Text("Loading...")
         }
     }
 }
