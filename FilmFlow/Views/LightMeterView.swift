107964e77646a35c7595ef1aac5d66ccc005c45c
Ying-Shan Lin <yslinear@gmail.com>
2024-04-05 15:57:39 +0800

    refactor(視圖): 改進LightMeterView的佈局及新增資訊行

--- FilmFlow/Views/LightMeterView.swift
+++ FilmFlow/Views/LightMeterView.swift
@@ -15,14 +15,21 @@ struct LightMeterView: View {
 
     var body: some View {
         if let image = manager.frame {
-            VStack() {
-                Image(image, scale: 1.0, orientation: .up, label: Text("frame"))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxHeight: 300)
-                Text("ISO: \(manager.videoDevice?.iso ?? 0)")
-                Text("Aperture: \(Float((manager.videoDevice?.lensAperture)!))")
-                Text("Sutter Speed: \(Float(CMTimeGetSeconds((manager.videoDevice?.exposureDuration)!)))")
+            Grid {
+                GridRow {
+                    VStack(alignment: .leading) {
+                        Text("ISO: \(manager.videoDevice?.iso ?? 0)")
+                        Text("Aperture: \(Float((manager.videoDevice?.lensAperture)!))")
+                        Text("Sutter Speed: \(Float(CMTimeGetSeconds((manager.videoDevice?.exposureDuration)!)))")
+                    }
+                    Image(image, scale: 1.0, orientation: .up, label: Text("frame"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxHeight: 300)
+                }
+                GridRow {
+                    Text("Other Info")
+                }
             }
         } else {
             Text("Loading...")
