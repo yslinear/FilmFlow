46a10e179f9647d92072e331403c457773f741c1
Ying-Shan Lin <yslinear@gmail.com>
2024-04-05 16:22:57 +0800

    feat(FilmFlow): 新增曝光值(EV)計算功能

--- FilmFlow/Views/LightMeterView.swift
+++ FilmFlow/Views/LightMeterView.swift
@@ -12,6 +12,7 @@ import AVFoundation
 
 struct LightMeterView: View {
     @StateObject var manager = CameraManager()
+    @State private var ev = 0.0
 
     var body: some View {
         if let image = manager.frame {
@@ -25,12 +26,23 @@ struct LightMeterView: View {
                     Image(image, scale: 1.0, orientation: .up, label: Text("frame"))
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(maxHeight: 300)
                 }
                 GridRow {
-                    Text("Other Info")
+                    VStack {
+                        Text("Other Info")
+                        Text("EV: \(ev)")
+                    }
+                    Button(action: {
+                        let iso = manager.videoDevice?.iso
+                        let aperture = manager.videoDevice?.lensAperture
+                        let shutterSpeed = CMTimeGetSeconds((manager.videoDevice?.exposureDuration)!)
+
+                        ev = Double(log2(Float(iso!)) + log2(Float(aperture!)) - log2(Float(shutterSpeed)))
+                    }) {
+                        Text("Calculate EV")
+                    }
                 }
-            }
+            }.padding()
         } else {
             Text("Loading...")
         }
